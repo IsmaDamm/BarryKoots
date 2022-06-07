56cc2793d88647289f9be3e2c697bcd16b9dd556
ams <abel99ms@gmail.com>
2022-06-07 11:22:03 +0200

    ok

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@
 	$mensaje = " Nombre: ".$nombre."\n Email:" .$email. "\n Mensaje:".$_POST['message'];
 
 
-	if(mail("abel.millan@mataro.epiaedu.cat", $email."\n".$asunto, $mensaje)){
+	if(mail("abel.millan@mataro.epiaedu.cat", $asunto, $mensaje)){
 		echo "Correo enviado";
 	}
 ?>
