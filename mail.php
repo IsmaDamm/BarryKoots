7bc411bb5327097a4ec7dd9c3a1bd8139755d187
ismaDamm <issmaelhassouni@gmail.com>
2022-06-08 11:48:25 +0200

    a medias

--- mail.php
+++ mail.php
@@ -1,15 +1,3 @@
- /*<?php
-  $nombre = $_POST['name'];
-	$email = $_POST['email'];
-	$asunto = 'Formulario Rellenado';
-	$mensaje = " Nombre: ".$nombre."\n Email:" .$email. "\n Mensaje:".$_POST['message'];
-
-
-	if(mail("abel.millan@mataro.epiaedu.cat", $asunto, $mensaje)){
-		echo "Correo enviado";
-	}
-?>*/
-
 <?php
   if(isset( $_POST['name']))
   $name = $_POST['name'];
