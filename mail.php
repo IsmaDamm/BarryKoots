c6edab5f4b021ff139a927c6726d297ce78cad56
ams <abel99ms@gmail.com>
2022-06-20 09:56:54 +0200

    msg

--- mail.php
+++ mail.php
@@ -2,10 +2,9 @@
   $nombre = $_POST['name'];
 	$email = $_POST['email'];
 	$asunto = 'Formulario Rellenado';
-	$mensaje = "Nombre: ".$nombre."<br> Email: $email<br> Mensaje:".$_POST['message'];
+	$mensaje = " Nombre: ".$nombre."\n Email:" .$email. "\n Mensaje:".$_POST['message'];
 
 
-	if(mail('abel.millan@mataro.epiaedu.cat', $asunto, $mensaje)){
-		echo "Correo enviado";
-	}
+	mail("abel.millan@mataro.epiaedu.cat", $asunto, $mensaje);
+	echo "Correo enviado";
 ?>
