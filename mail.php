f0c8ed591057c31a220d213cbabae7df2e0a8f01
ams <abel99ms@gmail.com>
2022-06-21 09:40:58 +0200

    finish

--- mail.php
+++ mail.php
@@ -5,6 +5,6 @@
    $mensaje = " Nombre: ".$nombre."\n Email:" .$email. "\n Mensaje:".$_POST['message'];
 
 
-   mail("nijenhuis.barry@gmail.com", $asunto, $mensaje);
+   mail("abel99ms@gmail.com", $asunto, $mensaje);
    echo "Correo enviado";
 ?>	
